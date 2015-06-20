21ff3f59aca234cf0e8a58fb6b1e2d47bde9429e
Gordon Ross <gwr@nexenta.com>
2015-06-19 23:36:36 -0400

    NEX-4026 panic in smb_session_send NEX-3738 Should support SMB2_CAP_LARGE_MTU (uio bug) Reviewed by: Alek Pinchuk <alek.pinchuk@nexenta.com> Reviewed by: Yuri Pankov <yuri.pankov@nexenta.com>

--- usr/src/lib/libfakekernel/common/ksocket.c
+++ usr/src/lib/libfakekernel/common/ksocket.c
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright 2014 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.
  */
 
 #include <sys/types.h>
@@ -295,7 +295,8 @@ int
 ksocket_sendmsg(ksocket_t ks, struct nmsghdr *msg, int flags,
     size_t *sent, struct cred *cr)
 {
-	ssize_t error;
+	uio_t uio;
+	ssize_t len;
 
 	/* All Solaris components should pass a cred for this operation. */
 	ASSERT(cr != NULL);
@@ -306,15 +307,33 @@ ksocket_sendmsg(ksocket_t ks, struct nmsghdr *msg, int flags,
 		return (ENOTSOCK);
 	}
 
-	error = sendmsg(KSTOSO(ks), msg, flags);
-	if (error < 0) {
+	len = sendmsg(KSTOSO(ks), msg, flags);
+	if (len < 0) {
 		if (sent != NULL)
 			*sent = 0;
 		return (errno);
 	}
 
+	/*
+	 * The user-level sendmsg() does NOT update msg->iov like
+	 * ksocket_sendmsg().  It's unclear whether that's a bug
+	 * or if that was intentional.  Anyway, update it here.
+	 */
+	if (msg->msg_iov != NULL) {
+		bzero(&uio, sizeof (uio));
+		uio.uio_iov = msg->msg_iov;
+		uio.uio_iovcnt = msg->msg_iovlen;
+		uio.uio_resid = len;
+
+		uioskip(&uio, len);
+		ASSERT(uio.uio_resid == 0);
+
+		msg->msg_iov = uio.uio_iov;
+		msg->msg_iovlen = uio.uio_iovcnt;
+	}
+
 	if (sent != NULL)
-		*sent = (size_t)error;
+		*sent = (size_t)len;
 	return (0);
 }
 
