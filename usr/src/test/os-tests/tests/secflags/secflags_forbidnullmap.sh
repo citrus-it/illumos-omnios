b164ee0b545ddf1788a837d0c417128124c0ad0f
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2016-12-17 10:04:33 -0500

    lib: nuke 0@0

--- usr/src/test/os-tests/tests/secflags/secflags_forbidnullmap.sh
+++ usr/src/test/os-tests/tests/secflags/secflags_forbidnullmap.sh
@@ -12,14 +12,22 @@
 #
 
 # Copyright 2015, Richard Lowe.
+# Copyright 2016, Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
 
 /usr/bin/psecflags -s forbidnullmap $$
 
-LD_PRELOAD=0@0.so.1 /usr/bin/sleep 100000 &
-pid=$!
+check() {
+	/opt/os-tests/tests/secflags/0sleep-$1 100000 &
+	pid=$!
 
-ret=0
-(pmap $pid | grep -q '^00000000 ') && ret=1
-kill -9 $pid
+	ret=0
+	(pmap $pid | grep -q "^$2 ") && ret=1
+	kill -9 $pid
 
-exit $ret
+	[ $ret -ne 0 ] && exit $ret
+}
+
+check 32 "00000000"
+check 64 "0000000000000000"
+
+exit 0
