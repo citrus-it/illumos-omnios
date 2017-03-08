a4c575a4697e8b6feefd614f10cdae72b10fa5c4
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2017-03-08 14:38:21 -0500

    rcm_daemon: remove clustering support from SUNW_ip_anon_rcm

--- usr/src/cmd/rcm_daemon/common/ip_anon_rcm.c
+++ usr/src/cmd/rcm_daemon/common/ip_anon_rcm.c
@@ -43,7 +43,6 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/cladm.h>
 #include <sys/file.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -66,7 +65,6 @@
 #define	IP_OFFLINE_ERR	gettext("Invalid operation: IP cannot be offlined")
 #define	IP_REMOVE_ERR	gettext("Invalid operation: IP cannot be removed")
 #define	IP_REG_FAIL	gettext("Registration Failed")
-#define	IP_NO_CLUSTER	gettext("Could not read cluster network addresses")
 
 #define	IP_FLAG_NEW	0x00
 #define	IP_FLAG_REG	0x01
@@ -88,12 +86,6 @@ static int		ip_anon_online(rcm_handle_t *, char *, id_t, uint_t,
 			    char **, rcm_info_t **);
 static int		ip_anon_remove(rcm_handle_t *, char *, id_t, uint_t,
 			    char **, rcm_info_t **);
-
-static int		exclude_ipv4(cladm_netaddrs_t exclude_addrs,
-			    ipaddr_t address);
-static int		exclude_ipv6(cladm_netaddrs_t exclude_addrs,
-			    uint32_t address[4]);
-
 
 typedef struct ip_status {
 	int			flags;
@@ -158,115 +150,15 @@ rcm_mod_fini()
 static int
 ip_anon_register(rcm_handle_t *hdl)
 {
-	int bootflags;
 	struct ifaddrlist *al = NULL, *al6 = NULL;
 	char errbuf[ERRBUFSIZE];
 	char treg[IP_REG_SIZE], tstr[IP_REG_SIZE];
-	cladm_netaddrs_t exclude_addrs;
 	int num_ifs, num_ifs6,  i, ret;
-	uint32_t num_exclude_addrs = 0;
 	ip_status_t *tlist, *tentry;
 
 	(void) mutex_lock(&ip_list_lock);
 
 	rcm_log_message(RCM_DEBUG, "ip_anon: registration refresh.\n");
-
-	exclude_addrs.cladm_num_netaddrs = 0;
-
-	if (_cladm(CL_INITIALIZE, CL_GET_BOOTFLAG, &bootflags) != 0) {
-		rcm_log_message(RCM_ERROR,
-		    gettext("unable to check cluster status\n"));
-		(void) mutex_unlock(&ip_list_lock);
-		return (RCM_FAILURE);
-	}
-
-	rcm_log_message(RCM_DEBUG,
-	    "ip_anon: cladm bootflags=%d\n", bootflags);
-
-	if (bootflags == 3) {
-
-		/* build the exclusion list */
-
-		if ((ret = _cladm(CL_CONFIG, CL_GET_NUM_NETADDRS,
-		    &num_exclude_addrs)) == 0) {
-			exclude_addrs.cladm_num_netaddrs = num_exclude_addrs;
-
-			if (num_exclude_addrs == 0)
-				rcm_log_message(RCM_DEBUG,
-				    "ip_anon: no addresses excluded\n");
-			else {
-				if ((exclude_addrs.cladm_netaddrs_array =
-				    malloc(sizeof (cladm_netaddr_entry_t) *
-				    (num_exclude_addrs))) == NULL) {
-					rcm_log_message(RCM_ERROR,
-					    gettext("out of memory\n"));
-					(void) mutex_unlock(&ip_list_lock);
-					return (RCM_FAILURE);
-				}
-
-				if ((ret = _cladm(CL_CONFIG,
-				    CL_GET_NETADDRS, &exclude_addrs))
-				    != 0) {
-					rcm_log_message(RCM_ERROR,
-					    IP_NO_CLUSTER);
-					(void) mutex_unlock(&ip_list_lock);
-					return (RCM_FAILURE);
-				}
-			}
-
-		} else {
-			if ((ret != 0) && (errno == EINVAL)) {
-				rcm_log_message(RCM_DEBUG,
-				    "no _cladm() backend to get addrs\n");
-			} else {
-				rcm_log_message(RCM_ERROR, IP_NO_CLUSTER);
-				(void) mutex_unlock(&ip_list_lock);
-				return (RCM_FAILURE);
-			}
-		}
-		rcm_log_message(RCM_DEBUG,
-		    "cladm returned %d errno=%d\n", ret, errno);
-
-		rcm_log_message(RCM_DEBUG,
-		    "ip_anon: num exclude addrs: %d\n",
-		    exclude_addrs.cladm_num_netaddrs);
-
-		/* print the exclusion list for debugging purposes */
-
-		for (i = 0; i < exclude_addrs.cladm_num_netaddrs; i++) {
-			(void) strcpy(treg, "<UNKNOWN>");
-			(void) strcpy(tstr, "<UNKNOWN>");
-			if (exclude_addrs.cladm_netaddrs_array[i].\
-			    cl_ipversion == IPV4_VERSION) {
-				(void) inet_ntop(AF_INET,
-				    &exclude_addrs.cladm_netaddrs_array[i].
-				    cl_ipv_un.cl_ipv4.ipv4_netaddr,
-				    treg, INET_ADDRSTRLEN);
-
-				(void) inet_ntop(AF_INET,
-				    &exclude_addrs.cladm_netaddrs_array[i].
-				    cl_ipv_un.cl_ipv4.ipv4_netmask,
-				    tstr, INET_ADDRSTRLEN);
-			}
-
-			if (exclude_addrs.cladm_netaddrs_array[i].\
-			    cl_ipversion == IPV6_VERSION) {
-				(void) inet_ntop(AF_INET6,
-				    &exclude_addrs.cladm_netaddrs_array[i].
-				    cl_ipv_un.cl_ipv6.ipv6_netaddr,
-				    treg, INET6_ADDRSTRLEN);
-
-				(void) inet_ntop(AF_INET6,
-				    &exclude_addrs.cladm_netaddrs_array[i].
-				    cl_ipv_un.cl_ipv6.ipv6_netmask,
-				    tstr, INET6_ADDRSTRLEN);
-			}
-			rcm_log_message(RCM_DEBUG, "IPV%d: %s %s\n",
-			    exclude_addrs.cladm_netaddrs_array[i].
-			    cl_ipversion, treg, tstr);
-
-		}
-	}
 
 	/* obtain a list of all IPv4 and IPv6 addresses in the system */
 
@@ -328,9 +220,6 @@ ip_anon_register(rcm_handle_t *hdl)
 			(void) mutex_unlock(&ip_list_lock);
 			return (RCM_FAILURE);
 		}
-
-		if (exclude_ipv4(exclude_addrs, al[i].addr.addr.s_addr))
-			tlist->flags |= IP_FLAG_CL;
 	}
 
 	/* IPv6 */
@@ -356,10 +245,6 @@ ip_anon_register(rcm_handle_t *hdl)
 			(void) mutex_unlock(&ip_list_lock);
 			return (RCM_FAILURE);
 		}
-
-		if (exclude_ipv6(exclude_addrs, al6[i].addr.addr6._S6_un.\
-		    _S6_u32))
-			tlist->flags |= IP_FLAG_CL;
 	}
 
 	rcm_log_message(RCM_DEBUG, "ip_anon: updating reg. state.\n");
@@ -602,95 +487,5 @@ deletereg(ip_status_t *entry)
 		tlist->next = entry->next;
 		free(entry);
 	}
-	return (0);
-}
-
-static int
-exclude_ipv4(cladm_netaddrs_t exclude_addrs, ipaddr_t address)
-{
-	int i;
-	char taddr[IP_REG_SIZE], tmask[IP_REG_SIZE], tmatch[IP_REG_SIZE];
-	ipaddr_t ipv4_netaddr, ipv4_netmask;
-
-	(void) inet_ntop(AF_INET, &address, taddr, INET_ADDRSTRLEN);
-
-	rcm_log_message(RCM_DEBUG, "ip_anon: exclude_ipv4 (%s, %d)\n",
-	    taddr, exclude_addrs.cladm_num_netaddrs);
-	/*
-	 * If this falls in the exclusion list, the IP_FLAG_CL
-	 * bit should be set for the adapter.
-	 */
-	for (i = 0; i < exclude_addrs.cladm_num_netaddrs; i++) {
-		if (exclude_addrs.cladm_netaddrs_array[i].\
-		    cl_ipversion == IPV4_VERSION) {
-
-			ipv4_netaddr = exclude_addrs.\
-			    cladm_netaddrs_array[i].cl_ipv_un.cl_ipv4.\
-			    ipv4_netaddr;
-			ipv4_netmask = exclude_addrs.\
-			    cladm_netaddrs_array[i].cl_ipv_un.cl_ipv4.\
-			    ipv4_netmask;
-
-			(void) inet_ntop(AF_INET, &ipv4_netaddr, tmatch,
-			    INET_ADDRSTRLEN);
-			(void) inet_ntop(AF_INET, &ipv4_netmask, tmask,
-			    INET_ADDRSTRLEN);
-
-			if ((address & ipv4_netmask) == ipv4_netaddr) {
-				rcm_log_message(RCM_DEBUG,
-				    "ip_anon: matched %s:%s => %s\n",
-				    taddr, tmask, tmatch);
-				return (1);
-			}
-		}
-	}
-	rcm_log_message(RCM_DEBUG, "ip_anon: no match for %s\n",
-	    taddr);
-	return (0);
-}
-
-static int
-exclude_ipv6(cladm_netaddrs_t exclude_addrs, uint32_t address[4])
-{
-	int i, j, numequal;
-	uint32_t addr[4], ipv6_netaddr[4], ipv6_netmask[4];
-	char taddr[IP_REG_SIZE], tmask[IP_REG_SIZE], tmatch[IP_REG_SIZE];
-
-	(void) inet_ntop(AF_INET6, address, taddr, INET6_ADDRSTRLEN);
-
-	/*
-	 * If this falls in the exclusion list, the IP_FLAG_CL
-	 * bit should be set for the adapter.
-	 */
-
-	for (i = 0; i < exclude_addrs.cladm_num_netaddrs; i++) {
-		if (exclude_addrs.cladm_netaddrs_array[i].\
-		    cl_ipversion == IPV6_VERSION) {
-			numequal = 0;
-			for (j = 0; j < 4; j++) {
-				ipv6_netaddr[j] = exclude_addrs.\
-				    cladm_netaddrs_array[i].\
-				    cl_ipv_un.cl_ipv6.ipv6_netaddr[j];
-
-				ipv6_netmask[j] = exclude_addrs.\
-				    cladm_netaddrs_array[i].\
-				    cl_ipv_un.cl_ipv6.ipv6_netmask[j];
-
-				addr[j] = address[j] & ipv6_netmask[j];
-				if (addr[j] == ipv6_netaddr[j])
-					numequal++;
-			}
-
-			(void) inet_ntop(AF_INET6, ipv6_netaddr, tmatch,
-			    INET6_ADDRSTRLEN);
-			(void) inet_ntop(AF_INET6, ipv6_netmask, tmask,
-			    INET6_ADDRSTRLEN);
-
-			if (numequal == 4)
-				return (1);
-		}
-	}
-	rcm_log_message(RCM_DEBUG, "ip_anon: no match for %s\n",
-	    taddr);
 	return (0);
 }
