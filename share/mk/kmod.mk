cbc5b103ff0ae7904ec8a237d3b63be4753bd3d4
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2017-06-28 17:12:45 +0300

    kernel: stop building 32-bit kernel modules (bmake-ified only)
    We still build the kernel modules built by the legacy dmake makefiles.

--- share/mk/kmod.mk
+++ share/mk/kmod.mk
@@ -4,17 +4,16 @@
 # See kernel/mk/kmod-build.mk for description of inputs used during the
 # build.  This makefile is responsible for making and cleaning up object
 # subdirectories.  All other tasks are handled by secondary makefile -
-# kmod-build.mk which we invoke for each of the bit-nesses.
+# kmod-build.mk.
 #
 # The config system tells us:
 #
 #   (1) whether to build this module at all
-#   (2) if we're supposed to build it, should we build 32-bit? 64-bit? both?
+#   (2) if we're supposed to build it, should we build 32-bit or 64-bit?
 #
-# The following two config vars address #2 above:
+# The following config variable addresses #2 above:
 #
-#   CONFIG_BUILD_KMOD_32 = y
-#   CONFIG_BUILD_KMOD_64 = y
+#   CONFIG_KERNEL_BITS = 64
 #
 # and the following config var tells us whether to bother with this
 # particular module:
@@ -37,40 +36,22 @@
 .include <unleashed.mk>
 .include <${SRCTOP}/Makefile.cfgparam>
 
-BUILD=
-.if defined(CONFIG_BUILD_KMOD_32) && ${CONFIG_BUILD_KMOD_32} == "y"
-BUILD += 32
-.endif
-.if defined(CONFIG_BUILD_KMOD_64) && ${CONFIG_BUILD_KMOD_64} == "y"
-BUILD += 64
-.endif
-
-all: ${BUILD:%=all-%}
-
-# we don't use a for loop to allow for more parallelism
-all-32:
-	@mkdir -p obj32
+all:
+	@mkdir -p obj${CONFIG_KERNEL_BITS}
 	@${MAKE} -f ${SRCTOP}/kernel/mk/kmod-build.mk all \
-		BITS=32 SRCTOP=${SRCTOP}
-
-all-64:
-	@mkdir -p obj64
-	@${MAKE} -f ${SRCTOP}/kernel/mk/kmod-build.mk all \
-		BITS=64 SRCTOP=${SRCTOP}
+		BITS=${CONFIG_KERNEL_BITS} SRCTOP=${SRCTOP}
 
 clean cleandir:
 	@${MAKE} -f ${SRCTOP}/kernel/mk/kmod-build.mk clean \
 		SRCTOP=${SRCTOP}
-	@rm -rf obj32 obj64
+	@rm -rf obj${CONFIG_KERNEL_BITS}
 
 install:
 	@${MAKE} -f ${SRCTOP}/kernel/mk/kmod-build.mk install-misc \
 		SRCTOP=${SRCTOP}
-.for bits in ${BUILD}
 	@${MAKE} -f ${SRCTOP}/kernel/mk/kmod-build.mk install \
-		BITS=${bits} SRCTOP=${SRCTOP}
-.endfor
+		BITS=${CONFIG_KERNEL_BITS} SRCTOP=${SRCTOP}
 
-.PHONY: all all-32 all-64 clean cleandir install
+.PHONY: all clean cleandir install
 
 .endif
