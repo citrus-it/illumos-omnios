afab6acada556da2361bfd076fde43027849a318
Lauri Tirkkonen <lotheac@iki.fi>
2017-07-03 16:49:01 +0300

    import libtls (LibreSSL 2.5.4)
    not part of build yet.

---
+++ lib/libtls/compat.mk
@@ -0,0 +1,30 @@
+.include <unleashed.mk>
+LCRYPTO_SRC=	${SRCTOP}/lib/libcrypto
+CPPFLAGS+=	-D__BEGIN_HIDDEN_DECLS= -D__END_HIDDEN_DECLS=
+CPPFLAGS+=	-I${LCRYPTO_SRC}/compat/include
+# see libressl-portable/portable:m4/disable-compiler-warnings.m4 
+CFLAGS+=	-Wno-pointer-sign
+SRCS+=		timingsafe_memcmp.c
+.PATH:		${LCRYPTO_SRC}/compat
+
+BSDOBJDIR?=	${.OBJDIR:tA:H:H}
+SHLIB_LDADD?=	${LDADD}
+
+INSTALL_COPY?=	${COPY}
+SHAREMODE?=	444
+
+# we don't use MAPFILE_VERS here, because that would cause us to ignore
+# shlib_version
+SHLIB_LDADD+=	-M${.OBJDIR}/mapfile
+mapfile: ${VERSION_SCRIPT}
+	{ echo '$$mapfile_version 2'; \
+	    printf 'SYMBOL_SCOPE '; \
+	    cat ${VERSION_SCRIPT}; } > $@
+CLEANFILES+=	mapfile
+all: mapfile
+
+# XXX includes need to be available in DESTDIR if DESTDIR is specified since
+# the mk files set -isysroot in that case
+.ifdef DESTDIR
+beforebuild: includes
+.endif
