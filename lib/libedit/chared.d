0bd14da29d61791377d945d31bbc366f4971f6c8
Lauri Tirkkonen <lotheac@iki.fi>
2017-10-03 12:33:17 +0300

    import libedit from OpenBSD
    not built yet.

---
+++ lib/libedit/chared.d
@@ -0,0 +1,113 @@
+chared.o chared.po chared.so chared.do: chared.c config.h sys.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/types.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/machine/endian.h \
+  /usr/include/ctype.h /usr/include/stdlib.h /usr/include/string.h \
+  /usr/include/strings.h el.h histedit.h /usr/include/stdint.h \
+  /usr/include/wchar.h /usr/include/wctype.h chartype.h tty.h \
+  /usr/include/termios.h /usr/include/sys/ttycom.h \
+  /usr/include/sys/ioccom.h /usr/include/sys/ttydefaults.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h prompt.h keymacro.h \
+  terminal.h refresh.h chared.h search.h hist.h map.h sig.h \
+  /usr/include/signal.h /usr/include/sys/signal.h \
+  /usr/include/machine/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/sys/time.h /usr/include/sys/select.h /usr/include/time.h \
+  /usr/include/sys/_time.h common.h fcns.h
+
+config.h:
+
+sys.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/machine/cdefs.h:
+
+/usr/include/stdio.h:
+
+/usr/include/sys/_null.h:
+
+/usr/include/sys/_types.h:
+
+/usr/include/machine/_types.h:
+
+/usr/include/sys/types.h:
+
+/usr/include/sys/endian.h:
+
+/usr/include/sys/_endian.h:
+
+/usr/include/machine/endian.h:
+
+/usr/include/ctype.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/string.h:
+
+/usr/include/strings.h:
+
+el.h:
+
+histedit.h:
+
+/usr/include/stdint.h:
+
+/usr/include/wchar.h:
+
+/usr/include/wctype.h:
+
+chartype.h:
+
+tty.h:
+
+/usr/include/termios.h:
+
+/usr/include/sys/ttycom.h:
+
+/usr/include/sys/ioccom.h:
+
+/usr/include/sys/ttydefaults.h:
+
+/usr/include/unistd.h:
+
+/usr/include/sys/unistd.h:
+
+prompt.h:
+
+keymacro.h:
+
+terminal.h:
+
+refresh.h:
+
+chared.h:
+
+search.h:
+
+hist.h:
+
+map.h:
+
+sig.h:
+
+/usr/include/signal.h:
+
+/usr/include/sys/signal.h:
+
+/usr/include/machine/signal.h:
+
+/usr/include/sys/siginfo.h:
+
+/usr/include/sys/time.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/time.h:
+
+/usr/include/sys/_time.h:
+
+common.h:
+
+fcns.h:
