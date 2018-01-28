2c54fc604be0e4ae0effe0fd6ad8110a2d64e244
Lauri Tirkkonen <lotheac@iki.fi>
2018-01-28 12:46:00 +0200

    import less(1)
    taken from openbsd as they already use bsd make; gdamore's upstream
    build system is more complicated.

---
+++ bin/less/screen.d
@@ -0,0 +1,113 @@
+screen.o: /home/lauri/bsd/src/usr.bin/less/less/../screen.c \
+  /usr/include/sys/ioctl.h /usr/include/sys/ttycom.h \
+  /usr/include/sys/ioccom.h /usr/include/sys/filio.h \
+  /usr/include/sys/sockio.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/err.h \
+  /usr/include/machine/_types.h /usr/include/term.h \
+  /usr/include/termios.h /usr/include/sys/_types.h \
+  /usr/include/sys/ttydefaults.h \
+  /home/lauri/bsd/src/usr.bin/less/less/../cmd.h \
+  /home/lauri/bsd/src/usr.bin/less/less/../less.h \
+  /home/lauri/bsd/src/usr.bin/less/less/../defines.h \
+  /usr/include/sys/types.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/machine/endian.h \
+  /usr/include/ctype.h /usr/include/fcntl.h /usr/include/libgen.h \
+  /usr/include/limits.h /usr/include/sys/limits.h \
+  /usr/include/machine/limits.h /usr/include/sys/syslimits.h \
+  /usr/include/signal.h /usr/include/sys/signal.h \
+  /usr/include/machine/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/sys/time.h /usr/include/sys/select.h /usr/include/time.h \
+  /usr/include/sys/_null.h /usr/include/sys/_time.h /usr/include/stdio.h \
+  /usr/include/stdlib.h /usr/include/string.h /usr/include/strings.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h /usr/include/wctype.h \
+  /home/lauri/bsd/src/usr.bin/less/less/../funcs.h /usr/include/regex.h
+
+/usr/include/sys/ioctl.h:
+
+/usr/include/sys/ttycom.h:
+
+/usr/include/sys/ioccom.h:
+
+/usr/include/sys/filio.h:
+
+/usr/include/sys/sockio.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/machine/cdefs.h:
+
+/usr/include/err.h:
+
+/usr/include/machine/_types.h:
+
+/usr/include/term.h:
+
+/usr/include/termios.h:
+
+/usr/include/sys/_types.h:
+
+/usr/include/sys/ttydefaults.h:
+
+/home/lauri/bsd/src/usr.bin/less/less/../cmd.h:
+
+/home/lauri/bsd/src/usr.bin/less/less/../less.h:
+
+/home/lauri/bsd/src/usr.bin/less/less/../defines.h:
+
+/usr/include/sys/types.h:
+
+/usr/include/sys/endian.h:
+
+/usr/include/sys/_endian.h:
+
+/usr/include/machine/endian.h:
+
+/usr/include/ctype.h:
+
+/usr/include/fcntl.h:
+
+/usr/include/libgen.h:
+
+/usr/include/limits.h:
+
+/usr/include/sys/limits.h:
+
+/usr/include/machine/limits.h:
+
+/usr/include/sys/syslimits.h:
+
+/usr/include/signal.h:
+
+/usr/include/sys/signal.h:
+
+/usr/include/machine/signal.h:
+
+/usr/include/sys/siginfo.h:
+
+/usr/include/sys/time.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/time.h:
+
+/usr/include/sys/_null.h:
+
+/usr/include/sys/_time.h:
+
+/usr/include/stdio.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/string.h:
+
+/usr/include/strings.h:
+
+/usr/include/unistd.h:
+
+/usr/include/sys/unistd.h:
+
+/usr/include/wctype.h:
+
+/home/lauri/bsd/src/usr.bin/less/less/../funcs.h:
+
+/usr/include/regex.h:
