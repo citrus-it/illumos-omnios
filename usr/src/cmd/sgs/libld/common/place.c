e586373f9e2c9c2c13ba3c8705fcba7b3542ac1b
Lauri Tirkkonen <lotheac@iki.fi>
2019-05-03 18:48:54 +0000

    ld: relax rw+ro eh_frame section merging requirements
    the output section does not actually need to be relocatable for this to be
    ok; gnu ld will happily merge
    	CONTENTS, ALLOC, LOAD, RELOC, READONLY, DATA and
    	CONTENTS, ALLOC, LOAD, DATA
    eh_frame sections into
    	CONTENTS, ALLOC, LOAD, READONLY, DATA
    
    this is necessary at least for bootstrap, because gcc's crt files don't have
    READONLY eh_frame presently.

--- usr/src/cmd/sgs/libld/common/place.c
+++ usr/src/cmd/sgs/libld/common/place.c
@@ -1067,8 +1067,7 @@ ld_place_section(Ofl_desc *ofl, Is_desc *isp, Place_path_info *path_info,
 	shflagmask =
 	    (ofl->ofl_flags & FLG_OF_RELOBJ) ? ALL_SHF_ORDER : ALL_SHF_IGNORE;
 
-	if (is_ehframe && (ofl->ofl_flags & FLG_OF_RELOBJ) &&
-	    ld_targ.t_m.m_mach == EM_386)
+	if (is_ehframe && ld_targ.t_m.m_mach == EM_386)
 		shflagmask |= SHF_WRITE;
 
 	/*
