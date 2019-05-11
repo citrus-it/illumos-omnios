95dac590498451c4c1935a102f34db7eff131e33
Lauri Tirkkonen <lotheac@iki.fi>
2019-05-11 00:21:13 +0000

    mk: use explicit -m32 for i386 now that -m64 is the default

--- share/mk/config.mk
+++ share/mk/config.mk
@@ -3,8 +3,9 @@ MK_PROFILE?=	no
 MK_PICLIB?=	no
 
 .if ${MACHINE} == "amd64"
-CFLAGS+=	-m64
-LDFLAGS+=	-m64
-AFLAGS+=	-m64
 LIBDIR?=	${libprefix}/lib/amd64
+.elif ${MACHINE} == "i386"
+CFLAGS+=	-m32
+LDFLAGS+=	-m32
+AFLAGS+=	-m32
 .endif
