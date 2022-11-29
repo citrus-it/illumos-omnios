0e1309f3cf97e6f17fe9bf366d9d452491b91ce9
Andy Fiddaman <illumos@fiddaman.net>
2022-11-29 09:57:40 +0000

    bhyve basl: Use GCC pragmas.

--- usr/src/cmd/bhyve/basl.h
+++ usr/src/cmd/bhyve/basl.h
@@ -6,10 +6,10 @@
 
 #pragma once
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <contrib/dev/acpica/include/acpi.h>
-#pragma clang diagnostic pop
+#pragma GCC diagnostic pop
 
 #ifndef	__FreeBSD__
 /* Until the in-gate ACPI is updated, map the new name to the old. */
