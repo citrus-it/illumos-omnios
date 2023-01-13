978f11a8e713b07f6d3c4d2f5200032b1c3a9f77
Andy Fiddaman <illumos@fiddaman.net>
2023-01-13 11:47:26 +0000

    Reduce AP spinup diffs

--- usr/src/cmd/bhyve/bhyverun.h
+++ usr/src/cmd/bhyve/bhyverun.h
@@ -55,9 +55,6 @@ struct vm_exit;
 void *paddr_guest2host(struct vmctx *ctx, uintptr_t addr, size_t len);
 
 void fbsdrun_addcpu(struct vmctx *ctx, int newcpu, bool suspend);
-#ifndef __FreeBSD__
-void fbsdrun_set_capabilities(struct vmctx *ctx, int cpu);
-#endif
 
 int  fbsdrun_virtio_msix(void);
 
