11fc3717b85140905b1b3cc411b49c07f971eee6
Andy Fiddaman <illumos@fiddaman.net>
2026-09-09 17:46:02 +0100

    oxide: hat_tlb_inval_range() should not scan every possible CPU id
    The oxide copy of the i86pc change of the same name: walk the source
    cpuset a word at a time with bt_getlowbit(), build the target set
    directly and count targets instead of comparing full-width cpusets.

--- usr/src/uts/oxide/vm/hat_i86.c
+++ usr/src/uts/oxide/vm/hat_i86.c
@@ -30,6 +30,7 @@
  * Copyright 2018 Joyent, Inc.  All rights reserved.
  * Copyright (c) 2014, 2015 by Delphix. All rights reserved.
  * Copyright 2022 Tintri by DDN, Inc. All rights reserved.
+ * Copyright 2026 Oxide Computer Company
  */
 
 /*
@@ -2420,11 +2421,10 @@ void
 hat_tlb_inval_range(hat_t *hat, tlb_range_t *in_range)
 {
 	extern int	flushes_require_xcalls;	/* from mp_startup.c */
-	cpuset_t	justme;
+	cpuset_t	*cpus;
 	cpuset_t	cpus_to_shootdown;
 	tlb_range_t	range = *in_range;
-	cpuset_t	check_cpus;
-	cpu_t		*cpup;
+	uint_t		ntargets = 0;
 	int		c;
 
 	/*
@@ -2458,24 +2458,24 @@ hat_tlb_inval_range(hat_t *hat, tlb_range_t *in_range)
 	 * Otherwise it's just CPUs currently executing in this hat.
 	 */
 	kpreempt_disable();
-	CPUSET_ONLY(justme, CPU->cpu_id);
 	if (hat == kas.a_hat)
-		cpus_to_shootdown = khat_cpuset;
+		cpus = &khat_cpuset;
 	else
-		cpus_to_shootdown = hat->hat_cpus;
+		cpus = &hat->hat_cpus;
 
 	/*
-	 * If any CPUs in the set are idle, just request a delayed flush
-	 * and avoid waking them up.
+	 * Build the set of CPUs to cross-call by walking the source set a
+	 * word at a time, so that the cost is proportional to the number of
+	 * CPUs in it rather than to the number of possible CPU ids. A CPU
+	 * that is idle is instead asked to flush its TLB when it next wakes,
+	 * which avoids waking it now.
 	 */
-	check_cpus = cpus_to_shootdown;
-	for (c = 0; c < NCPU && !CPUSET_ISNULL(check_cpus); ++c) {
+	CPUSET_ZERO(cpus_to_shootdown);
+	for (c = bt_getlowbit(CPUSET2BV(*cpus), 0, max_ncpus - 1); c != -1;
+	    c = bt_getlowbit(CPUSET2BV(*cpus), c + 1, max_ncpus - 1)) {
+		cpu_t *cpup = cpu[c];
 		ulong_t tlb_info;
 
-		if (!CPU_IN_SET(check_cpus, c))
-			continue;
-		CPUSET_DEL(check_cpus, c);
-		cpup = cpu[c];
 		if (cpup == NULL)
 			continue;
 
@@ -2488,12 +2488,18 @@ hat_tlb_inval_range(hat_t *hat, tlb_range_t *in_range)
 		}
 		if (tlb_info == (TLBIDLE_CPU_HALTED | TLBIDLE_INVAL_ALL)) {
 			HATSTAT_INC(hs_tlb_inval_delayed);
-			CPUSET_DEL(cpus_to_shootdown, c);
+			continue;
 		}
+		CPUSET_ADD(cpus_to_shootdown, c);
+		ntargets++;
 	}
 
-	if (CPUSET_ISNULL(cpus_to_shootdown) ||
-	    CPUSET_ISEQUAL(cpus_to_shootdown, justme)) {
+	/*
+	 * If there is nothing to cross-call, or the only target is this CPU,
+	 * do the work locally.
+	 */
+	if (ntargets == 0 ||
+	    (ntargets == 1 && CPU_IN_SET(cpus_to_shootdown, CPU->cpu_id))) {
 		(void) hati_demap_func((xc_arg_t)hat, (xc_arg_t)&range, 0);
 	} else {
 		CPUSET_ADD(cpus_to_shootdown, CPU->cpu_id);
