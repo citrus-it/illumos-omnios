12f3482274e6ce7712635f51699853edd5ad28f2
Andy Fiddaman <illumos@fiddaman.net>
2026-09-09 17:46:52 +0100

    oxide: cbe_fire() should not test every possible CPU id per tick
    The oxide copy of the i86pc change of the same name.

--- usr/src/uts/oxide/io/cbe.c
+++ usr/src/uts/oxide/io/cbe.c
@@ -23,7 +23,7 @@
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  *
- * Copyright 2023 Oxide Computer Co.
+ * Copyright 2026 Oxide Computer Company
  */
 
 #include <sys/systm.h>
@@ -102,10 +102,11 @@ cbe_fire(caddr_t arg1 __unused, caddr_t arg2 __unused)
 	cyclic_fire(cpu);
 
 	if (cbe_psm_timer_mode != TIMER_ONESHOT && me == 0 && !cross_call) {
-		for (i = 1; i < NCPU; i++) {
-			if (CPU_IN_SET(cbe_enabled, i)) {
-				send_dirint(i, CBE_HIGH_PIL);
-			}
+		ulong_t *bv = CPUSET2BV(cbe_enabled);
+
+		for (i = bt_getlowbit(bv, 1, max_ncpus - 1); i != -1;
+		    i = bt_getlowbit(bv, i + 1, max_ncpus - 1)) {
+			send_dirint(i, CBE_HIGH_PIL);
 		}
 	}
 
